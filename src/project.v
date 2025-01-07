8a0f9f5b4c50d420c7089df98662b52d9f5c0b22
Uri Shaked <uri@urishaked.com>
2025-01-07 15:54:31 +0200

    feat: verilog netlist for the top level module

--- src/project.v
+++ src/project.v
@@ -5,8 +5,6 @@
 
 `default_nettype none
 
-// just a stub to keep the Tiny Tapeout tools happy
-
 module tt_um_oscillating_bones (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
@@ -21,5 +19,62 @@ module tt_um_oscillating_bones (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+
+  wire osc_out_internal_3v3;
+  wire osc_out;
+  wire osc_div_2;
+  wire osc_div_4;
+  wire osc_div_8;
+  wire osc_out_3v3;
+
+  ring ring (.ROSC_OUT(osc_out_internal_3v3));
+
+  skullfet_inverter_5v_pwr level_shift (
+      .VDD(VDPWR),
+      .A  (osc_out_internal_3v3),
+      .Y  (osc_out)
+  );
+
+  freq_divider divider (
+      .IN(osc_out),
+      .ODIV2(osc_div_2),
+      .ODIV4(osc_div_4),
+      .ODIV8(osc_div_8)
+  );
+
+  skullfet_inverter_5v_pwr ua_buffer (
+      .VDD(VAPWR),
+      .A  (osc_out),
+      .Y  (osc_out_3v3)
+  );
+
+  assign uo_out[0] = osc_out;
+  assign uo_out[1] = osc_div_2;
+  assign uo_out[2] = osc_div_4;
+  assign uo_out[3] = osc_div_8;
+  assign uo_out[4] = VGND;
+  assign uo_out[5] = VGND;
+  assign uo_out[6] = VGND;
+  assign uo_out[7] = VGND;
+
+  assign uio_out[0] = VGND;
+  assign uio_out[1] = VGND;
+  assign uio_out[2] = VGND;
+  assign uio_out[3] = VGND;
+  assign uio_out[4] = VGND;
+  assign uio_out[5] = VGND;
+  assign uio_out[6] = VGND;
+  assign uio_out[7] = VGND;
+
+  assign uio_oe[0] = VGND;
+  assign uio_oe[1] = VGND;
+  assign uio_oe[2] = VGND;
+  assign uio_oe[3] = VGND;
+  assign uio_oe[4] = VGND;
+  assign uio_oe[5] = VGND;
+  assign uio_oe[6] = VGND;
+  assign uio_oe[7] = VGND;
+
+  assign ua[0] = osc_out_3v3;
 
 endmodule
