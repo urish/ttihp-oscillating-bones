ed966c8d29266143f7ef37fc24071528a33dc7b6
Uri Shaked <uri@urishaked.com>
2025-08-29 14:58:37 +0300

    fix(verilog): remove power pins
    They are not supported on IHP shuttles

--- src/project.v
+++ src/project.v
@@ -6,8 +6,6 @@
 `default_nettype none
 
 module tt_um_oscillating_bones (
-    input  wire       VGND,
-    input  wire       VDPWR,    // 1.2v power supply
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -36,27 +34,27 @@ module tt_um_oscillating_bones (
   assign uo_out[1] = osc_div_2;
   assign uo_out[2] = osc_div_4;
   assign uo_out[3] = osc_div_8;
-  assign uo_out[4] = VGND;
-  assign uo_out[5] = VGND;
-  assign uo_out[6] = VGND;
-  assign uo_out[7] = VGND;
+  assign uo_out[4] = 0;
+  assign uo_out[5] = 0;
+  assign uo_out[6] = 0;
+  assign uo_out[7] = 0;
 
-  assign uio_out[0] = VGND;
-  assign uio_out[1] = VGND;
-  assign uio_out[2] = VGND;
-  assign uio_out[3] = VGND;
-  assign uio_out[4] = VGND;
-  assign uio_out[5] = VGND;
-  assign uio_out[6] = VGND;
-  assign uio_out[7] = VGND;
+  assign uio_out[0] = 0;
+  assign uio_out[1] = 0;
+  assign uio_out[2] = 0;
+  assign uio_out[3] = 0;
+  assign uio_out[4] = 0;
+  assign uio_out[5] = 0;
+  assign uio_out[6] = 0;
+  assign uio_out[7] = 0;
 
-  assign uio_oe[0] = VGND;
-  assign uio_oe[1] = VGND;
-  assign uio_oe[2] = VGND;
-  assign uio_oe[3] = VGND;
-  assign uio_oe[4] = VGND;
-  assign uio_oe[5] = VGND;
-  assign uio_oe[6] = VGND;
-  assign uio_oe[7] = VGND;
+  assign uio_oe[0] = 0;
+  assign uio_oe[1] = 0;
+  assign uio_oe[2] = 0;
+  assign uio_oe[3] = 0;
+  assign uio_oe[4] = 0;
+  assign uio_oe[5] = 0;
+  assign uio_oe[6] = 0;
+  assign uio_oe[7] = 0;
 
 endmodule
